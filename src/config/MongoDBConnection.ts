e057229ab945ab11ca553362e06ca837bc45b265
thaisdev <thaishdzlpz@gmail.com>
2025-02-28 20:24:36 +0000

    feat: Add MongoDB initial config

---
+++ src/config/MongoDBConnection.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectToDatabase = async () => {
+    const { DB_USER, DB_PASSWORD, DB, DB_CLUSTER } = process.env;
+
+    const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB}.4e4li.mongodb.net/?retryWrites=true&w=majority&appName=${DB_CLUSTER}`;
+    try {
+        await mongoose.connect(uri);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch(error) {
+        console.error("Mongui no quiso contestar", error.message);
+    }
+}
+
+export default connectToDatabase;
